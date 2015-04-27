39d2ce3fb1c8a72615b94c5f6babc4a07fe3ec24
Munsterberg <jetrussell93@gmail.com>
2015-04-26 22:23:43 -0400

    small changes and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Small Simple CMS
+
+A small CMS project for teaching me some of the basics of PHP.
+
+P.S not to be confused with my node CMS project. 
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
 include_once('inc/connection.php');
+include_once('inc/article.php');
+
+$article = new Article;
+$articles = $article->fetch_all();
 
 ?>
 
@@ -14,9 +18,14 @@ include_once('inc/connection.php');
 <div class="container">
     <a href="index.php" id="brand">CMS</a>
 
+    <?php foreach($articles as $article) { ?>
     <ol>
-        <li><a href="article.php?id=1">Article Title</a> <small>created DATE</small></li>
+        <li><a href="article.php?id=<?php echo $article['article_id']; ?>">
+                <?php echo $article['article_title']; ?>
+            </a>
+            <small><?php echo date('l jS', $article['article_timestamp']) ?></small></li>
     </ol>
+    <?php } ?>
 </div>
 
 </body>
