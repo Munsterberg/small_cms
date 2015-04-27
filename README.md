dbc1cd257fa94aeb0a8ff9bb596f0455045e70c6
Munsterberg <jetrussell93@gmail.com>
2015-04-27 10:28:16 -0400

    Article pages displaying data

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A small CMS project for teaching me some of the basics of PHP.
 
-P.S not to be confused with my node CMS project. 
+P.S not to be confused with my node CMS project.
---
+++ article.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once('inc/connection.php');
+include_once('inc/article.php');
+
+$article = new Article;
+
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $data = $article->fetch_data($id);
+
+?>
+
+    <html>
+    <head>
+        <title>CMS</title>
+        <link rel="stylesheet" href="assets/style.css"/>
+    </head>
+    <body>
+
+    <div class="container">
+        <a href="index.php" id="brand">CMS</a>
+
+        <h3><?php echo $data['article_title']; ?></h3>
+        <small><?php echo date('l jS', $data['article_timestamp']) ?></small>
+        <p><?php echo $data['article_content']; ?></p>
+    </div>
+
+    </body>
+    </html>
+
+<?php
+
+} else {
+    header('Location: index.php');
+    exit();
+}
+
+?>
